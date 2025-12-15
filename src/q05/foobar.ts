02251cf3f2a099ee1ef8414d86b3197ee1477ad8
68011128_Sahapoom Rattanawiwatpong <68011128@kmitl.ac.th>
2025-12-15 09:49:12 +0700

    q01-q05

---
+++ src/q05/foobar.ts
@@ -0,0 +1,19 @@
+const input = +process.argv[2];
+if(Number.isNaN(input) || input <= 0){
+  process.exit
+}
+for(let i = 0; i <= input; i++){
+  if(i % 3 === 0 && i % 7 ===0){
+    console.log('FooBar')
+  }
+  else if(i % 3 === 0 ){
+    console.log('Foo')
+  }
+  else if(i % 7 === 0 ){
+    console.log('Bar')
+  }
+  else{
+      console.log(i);
+  }
+
+}
