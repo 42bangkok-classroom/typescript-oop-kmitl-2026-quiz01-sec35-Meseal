bc73df050fcf83b248e2d836c3e1a3f43598c003
68011128_Sahapoom Rattanawiwatpong <68011128@kmitl.ac.th>
2026-01-20 16:55:00 +0700

    dd

--- src/q02/atm.ts
+++ src/q02/atm.ts
@@ -6,7 +6,7 @@ const balance = Number(inputBalanceString);
 const amount = Number(inputAmountString);
 
 
-if (isNaN(balance) || isNaN(amount)) {
+if (isNaN(balance) || isNaN(amount) || !balance || !amount ) {
     console.log("Invalid Input");
     process.exit(1);
 }
